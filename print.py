385d6811d81b1cc6f2a8c0d2e1816ba6ff7380e6
ondieki1237 <makoriseth1237@gmail.com>
2022-09-28 03:41:46 +0300

    my first python codes

---
+++ print.py
@@ -0,0 +1,8 @@
+# this program takes in values and prints the output
+salutation = input('hi, how are you?')
+print('salutation')
+# a little excercise of printing two questions the name and the fav color of the name given
+name = input('kindly remind me your name?')
+print('Hi' + name)
+input('what is your fav color ' + name)
+print('thank you ' + name)
